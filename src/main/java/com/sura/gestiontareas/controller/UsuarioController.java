87bd4302a9df5c7ca45888ce2d002e110afe4221
jhoserpacheco <jhoserpacheco@gmail.com>
2023-11-22 10:15:42 -0500

    Agregando excepciones a usuario

--- src/main/java/com/sura/gestiontareas/controller/UsuarioController.java
+++ src/main/java/com/sura/gestiontareas/controller/UsuarioController.java
@@ -4,18 +4,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sura.gestiontareas.dto.UsuarioDTO;
 import com.sura.gestiontareas.service.iUsuarioService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@ControllerAdvice
 @RequestMapping("/gestiontareas/usuario")
 @CrossOrigin(origins = "*")
 @Log4j2
@@ -24,17 +25,16 @@ public class UsuarioController {
     private final iUsuarioService iUsuarioService;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> guardarUsuario (@RequestBody @Validated UsuarioDTO usuarioDto) throws Exception {
-        try{
+    public ResponseEntity<String> guardarUsuario (@RequestBody @Valid UsuarioDTO usuarioDto) {
+        try {
             UsuarioDTO usuarioResponse = iUsuarioService.crearUsuario(usuarioDto);
             return ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writeValueAsString(usuarioResponse));
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-
     @GetMapping(value = "/{idUsuario}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> buscarUsuario(@PathVariable("idUsuario") Integer idUsuario ){
+    public ResponseEntity<String> buscarUsuario(@Valid @PathVariable("idUsuario") Integer idUsuario ){
         UsuarioDTO usuarioResponse = iUsuarioService.buscarUsuario(idUsuario);
         try{
             return ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writeValueAsString(usuarioResponse));
@@ -44,7 +44,7 @@ public class UsuarioController {
     }
 
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> actualizarUsuario(@RequestBody @Validated UsuarioDTO
+    public ResponseEntity<String> actualizarUsuario(@RequestBody @Valid UsuarioDTO
                                                             usuarioDTO){
         try {
             UsuarioDTO usuarioResponse = iUsuarioService.actualizarUsuario(usuarioDTO);
@@ -55,8 +55,7 @@ public class UsuarioController {
     }
 
     @DeleteMapping(value = "/{idUsuario}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> borrarUsuario(@PathVariable("idUsuario") Integer
-                                                        idUsuario){
+    public ResponseEntity<String> borrarUsuario(@Valid @PathVariable("idUsuario") Integer idUsuario){
         String usuarioResponse = iUsuarioService.
                 borrarUsuario(iUsuarioService.buscarUsuario(idUsuario));
         try {
@@ -75,6 +74,11 @@ public class UsuarioController {
         } catch (Exception e) {
             return ResponseEntity.noContent().build();
         }
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 
 }
--- src/main/java/com/sura/gestiontareas/dto/UsuarioDTO.java
+++ src/main/java/com/sura/gestiontareas/dto/UsuarioDTO.java
@@ -1,6 +1,8 @@
 package com.sura.gestiontareas.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Builder
@@ -12,7 +14,9 @@ public class UsuarioDTO {
     @JsonProperty("id_usuario")
     private Integer idUsuario;
 
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String nombre;
 
+    @Email(message = "Email no puede estar vacío")
     private String email;
 }
